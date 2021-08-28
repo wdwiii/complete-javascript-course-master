00d7d320d885016eea07c449452ca162edc80fc8
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-28 12:56:47 -0400

    Initial commit
    - Add logic to log input value when chek button is clicked
    - Display error message if invalid input is received

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+/*
+- .querySelector can grab any element, class, or id
+- Use .textContent operator to view inner text of element
+- Multiple dot operators will read from left to right
+*/
+// console.log(document.querySelector('.message').textContent);
+
+// /*
+//  */
+// document.querySelector('.message').textContent = 'Correct Number';
+
+//document.querySelector('.number').textContent = 19;
+//document.querySelector('.score').textContent = 45;
+
+const checkBtn = document.querySelector('.check');
+const number = document.querySelector('.number');
+
+checkBtn.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess || guess < 1) {
+    document.querySelector('.message').textContent =
+      'Please enter a number between 1 and 20.';
+    document.querySelector('.message').style.color = 'red';
+  }
+});
