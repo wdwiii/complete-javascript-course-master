d7f4d965736dcd71b5a063222d1943f797617098
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-28 21:42:47 -0400

    Add changes to js
    - Declare secret number that generates a radom number
    - Add 'check' btn event listener
    - Create logic to change text and styles for win, lose, invalid answer
    -Add event listener for click of restart btn

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,16 +14,85 @@
 //document.querySelector('.number').textContent = 19;
 //document.querySelector('.score').textContent = 45;
 
+const restartBtn = document.querySelector('.restart');
 const checkBtn = document.querySelector('.check');
-const number = document.querySelector('.number');
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const message = document.querySelector('.message');
+
+let number = document.querySelector('.number');
+
+let score = 20;
+let scoreDisplay = document.querySelector('.score');
+
+let guess = document.querySelector('.guess').value;
+
+scoreDisplay.textContent = score;
 
 checkBtn.addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (!guess || guess < 1) {
-    document.querySelector('.message').textContent =
-      'Please enter a number between 1 and 20.';
-    document.querySelector('.message').style.color = 'red';
+  if (guess && guess > 0 && guess <= 20) {
+    //If the guess is a number between 1 and 20
+    if (guess !== secretNumber) {
+      //If the guess is not correct take 1 point away
+      score--;
+      scoreDisplay.textContent = score;
+
+      if (guess > secretNumber) {
+        //If the guess is greater than the secret number, display 'Too High'
+        message.textContent = 'Too High';
+      } else if (guess < secretNumber) {
+        //If the guess is less than the secret number, display 'Too Low'
+        message.textContent = 'Too Low';
+      }
+    } else if (guess === secretNumber) {
+      //If you guess correctly, display 'YOU WIN' in green, increasing the width of number container and reveal the number
+      number.textContent = secretNumber;
+      message.textContent = 'YOU WIN 🎉';
+      message.style.color = 'white';
+      document.querySelector('body').style.backgroundColor = '#00c324';
+      number.style.width = '25rem';
+      if (score > document.querySelector('.highscore').textContent)
+        document.querySelector('.highscore').textContent = score;
+    }
+  } else {
+    //If you guess a number less than 1 or greater than 20, take away a point and display error message in red
+    message.textContent = 'Please enter a number between 1 and 20.';
+    message.style.color = 'red';
+  }
+  if (score <= 0) {
+    //If score reaches zero, the display 'you lose'
+    scoreDisplay = 0;
+    number.textContent = secretNumber;
+    message.textContent = 'YOU LOSE!! :(';
+    message.style.color = 'red';
   }
 });
+
+const newRandom = function () {
+  //function to generate a new random number.
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  return secretNumber;
+};
+
+restartBtn.addEventListener('click', function () {
+  /*When reset button is clicked:
+  - Reset score
+  - Clear the number from input 
+  - Reset number content & width, message content & color
+  - Generate new random number
+  */
+  score = 20;
+  scoreDisplay.textContent = score;
+  document.querySelector('.guess').value = '';
+  number.textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  number.style.width = '15rem';
+  message.textContent = 'Start guessing...';
+  message.style.color = 'white';
+  newRandom();
+  console.log(secretNumber);
+});
+
+console.log(secretNumber);
