04829bb0f4fc9bde45e31fd437f2a8dde2bbed01
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-30 11:33:28 -0400

    Declare vars & call functions for starting cond's

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+// Selecting elements
+const p1ScoreElem = document.querySelector('#score--0');
+const p2ScoreElem = document.querySelector('#score--1');
+const diceElem = document.querySelector('.dice');
+
+// Functions
+const resetScore = function () {
+  p1ScoreElem.textContent = 0;
+  p2ScoreElem.textContent = 0;
+};
+const hideDice = function () {
+  if (!diceElem.classList.contains('hidden')) diceElem.classList.add('hidden');
+};
+
+// Starting Conditions
+resetScore();
+hideDice();
