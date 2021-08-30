a49e0b0e009e308673a36dedf639c6ff3185643d
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-30 09:40:39 -0400

    Declare modal vars and add click event for buttons

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+btnsOpenModal.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    console.log(btn.textContent);
+  });
+});
