bef17794a65f2fe4016ab49798048e7c7bc5e80d
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-30 10:16:25 -0400

    Refactor code for showing and hiding modal
    - Declare open and close modal functions as variables
    - Add event listener to show modal and overlay when btn is clicked
    - Add event listener to hide modal and overlay when x-btn is clicked

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,8 +4,31 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+//Refactored Code
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Shows the modal and overlay whenever one of the show modal buttons is clicked
 btnsOpenModal.forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    console.log(btn.textContent);
-  });
+  btn.addEventListener('click', openModal);
 });
+//Hides the modal and overlay whenever btnCloseModal (x button) OR overlay is clicked
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Original Code
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
