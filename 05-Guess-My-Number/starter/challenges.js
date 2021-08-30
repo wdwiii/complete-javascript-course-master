3c5e4f4f2c9c65c53c4f8097a2160394687927de
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-08-30 08:51:16 -0400

    Complete challenge number 1

---
+++ 05-Guess-My-Number/starter/challenges.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*==========================
+JavaScript in the Browser: DOM and Events
+
+Coding Challenge #1
+=============================
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+
+1. Select the element with the 'again' class and attach a click event handler
+
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK �*/
+
+// restartBtn.addEventListener('click', function () {
+//   /*When reset button is clicked:
+//   - Reset score
+//   - Clear the number from input
+//   - Reset number content & width, message content & color
+//   - Generate new random number
+//   */
+//   score = 20;
+//   scoreDisplay.textContent = score;
+//   document.querySelector('.guess').value = '';
+//   number.textContent = '?';
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   number.style.width = '15rem';
+//   message.textContent = 'Start guessing...';
+//   message.style.color = 'white';
+//   newRandom();
+//   console.log(secretNumber);
+// });
