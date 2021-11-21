9573ea968102b586de786f343a72680642f728b2
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-20 19:29:30 -0500

    Complete sect 100 - Primitives vs objects practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -12,13 +12,13 @@
 
 //Primitive Examples
 //Each var has a unique idenitfier address
-let age = 29;
+// let age = 29;
 //oldAge ponits to the memory address of age
-let oldAge = age;
+// let oldAge = age;
 //when the value is reassigned, the reassined variable (age) receives a new memory address in the call stack
-age = 44;
-console.log(age);
-console.log(oldAge);
+// age = 44;
+// console.log(age);
+// console.log(oldAge);
 
 //Reference Examples
 //Ref values are stored in the HEAP. It receives a unique identifier but it doesnot point directly to the newly created memory address in the heap.
@@ -27,15 +27,61 @@ console.log(oldAge);
 
 //The piece of memory in the call stack has a reference to the piece of memory in the heap, which holds the me object
 
-let me = {
-  name: 'Luna',
-  age: 2,
-};
+// let me = {
+// name: 'Luna',
+// age: 2,
+// };
 
-const friend = me;
-friend.age = 13;
+// const friend = me;
+// friend.age = 13;
 //friend and me actually point to the exact same object in the memory heap. Whenever we change something in this object so these are basically just two different identifiers pointing to the exact same value.
 
-console.log(friend, me);
+// console.log(friend, me);
 
 /////////////////////////////////////////
+//========================================
+//100. Primitives vs Objects in practice
+//========================================
+
+//Primitive types
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davis';
+
+// console.log(`Last Name: ${lastName}`);
+// console.log(`Old Last Name: ${oldLastName}`);
+
+//Reference types
+// const jessie = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+// };
+
+// const marriedJessica = jessie;
+
+// marriedJessica.lastName = 'Davis';
+
+// console.log(jessie);
+// console.log(marriedJessica);
+
+//Although the value of const variables cannot change. What actually needs to be constant is the value in the stack.
+
+//And in the above stack, the value only holds the reference to the underlying object that is stored in the heap.
+
+//Copying Objects
+const jessie = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 33,
+  hobbies: ['reading', 'surfing', 'drumming'],
+};
+
+const jessieCopy = Object.assign({}, jessie);
+//The object.assign method only creates a shallow copy. A shallow copy will only copy the properties in the first level.
+
+//Inner objects will still be tied to the original object
+
+jessieCopy.lastName = 'Davis';
+jessieCopy.hobbies.push('dog walking');
+console.log(`Before marriage: `, jessie);
+console.log(`After marriage: `, jessieCopy);
