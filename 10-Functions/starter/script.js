a9e51530c6186a093a23e88c5f346919e518e6f3
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2022-01-02 07:20:24 -0500

    Complete section 139 - Coding Challenge #2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -477,7 +477,33 @@ const boardPassengers = (numPassengers, waitTime) => {
   console.log(`Will start boarding in ${waitTime} seconds.`); //Run immmediately
 };
 
-boardPassengers(111, 3);
+//boardPassengers(111, 3);
 
 //The callback function was executed independently of the boardPassengers function.
 //But the callback function has access to all the variables that were in the variable environment in which it was created.
+
+//=============================
+// 139. Coding Challenge #2
+//=============================
+
+/*This is more of a thinking challenge than a coding challenge 🤓
+Your tasks:*/
+
+/*1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!*/
+
+/*2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+//This works because the function passed into the event listener has access to the variable environment in which it was created
+//This is another example of a closure
