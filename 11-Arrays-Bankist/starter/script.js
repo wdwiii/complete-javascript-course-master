492f0d1f62035ee9f9ac8f634badb89803981a87
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2022-01-02 14:39:39 -0500

    Complete 142 - Simple Array Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,70 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+//=============================
+// 142. Simple Array Methods
+//=============================
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//SLICE
+//~~~~~~~//
+//Returns a portion of an array
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+//A negative number can be passed as a parameter and the slice method will copy from the end of the array
+
+//console.log(arr.slice(1, -2));
+//Negative parameter can be used to specifiy the endpoint
+//In the above method, the array will copy starting from index 1, but excluding the last 2 elements
+
+//console.log(arr.slice());
+//Call .slice() without any arguments to create a copy of the array
+//Spread operator can also create shallow copy.
+//console.log([...arr]);
+
+//SPLICE
+//~~~~~~~//
+//Splice works similar to slice, but it modifies the original array
+//Typically with splice, the values deleted arent important, just that they are removed from the array.
+
+//arr.splice(2); //Extract first 2 elements
+//console.log(arr.splice(2)); //Extract and return first 2 elements
+//console.log(arr); //Returns mutated array w/ first 2 elements removed
+
+//arr.splice(-1); //Extracts last element
+//console.log(arr.splice(-1)); //Extract and return the extracted element
+//console.log(arr); //Returns mutated array w/ last element removed
+
+//console.log(arr);
+//arr.splice(1, 2); //Starts at index 1, removes 2 elements
+//console.log(arr); //Return mutated array with second and third array elements extracted
+
+//SLICE vs SPLICE
+// console.log(arr);
+// const arrSlice = arr.slice(1, 3); //starts to copy at index 1, cuts of at index 3 (i3 will be excluded)
+// console.log(arrSlice); //returns copied elements
+// console.log(arr); //original array unmutated
+
+// const arrSplice = arr.splice(1, 3); //starts to extract at index 1, extract exactly 3 elments
+// console.log(arrSplice); //returns extracted elements
+// console.log(arr); //mutated array with elements in index position 1, 2 & 3 removed
+
+//REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+//console.log(arr2.reverse());
+//console.log(arr2); //The reverse method actually mutates original array
+
+//CONTAT
+//The concat method joins two arrays w/o mutating
+let letters = arr.concat(arr2, ['k', 'l']); //Array(s) that is passed in will be added to end in the order they were passed
+//console.log(letters);
+//console.log([...arr, ...arr2, ...['k', 'l']]);
+
+//JOIN
+//Joins contents of array into a single string.
+let joinedLetters = letters.join(' + '); //What is passed in will be uses a seperator
+//console.log(joinedLetters);
