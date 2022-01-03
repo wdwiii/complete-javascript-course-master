79046c50a0d2ad17e710610d61f613aa94144d5c
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2022-01-03 08:33:45 -0500

    Complete section 148 - Coding Challenge #1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,67 +1,6 @@
 'use strict';
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
 
 //=============================
 // 142. Simple Array Methods
@@ -184,23 +123,146 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //==================================
 // 144. forEach with Maps and Sets
 //==================================
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach(function (value, key, map) {
-  console.log(`The abbreviation for the ${value} is ${key}`);
-});
+// currencies.forEach(function (value, key, map) {
+//   console.log(`The abbreviation for the ${value} is ${key}`);
+// });
 
-const currenciesUnique = new Set(['USD', 'USD', 'GBP', 'EUR', 'GBP']);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, key) {
-  console.log(`${key}: ${value}`);
-});
+// const currenciesUnique = new Set(['USD', 'USD', 'GBP', 'EUR', 'GBP']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, key) {
+//   console.log(`${key}: ${value}`);
+// });
 
 //Notes about sets
 //• Sets do not have a key
 //• The forEach accepts a second parameter (equiv to index or key) to keep the method consistent with maps and arrays
-//•The second param pased will set it's value to that of the firse
+//•The second param passed will set it's value to that of the first parameter
+
+//==================================
+// 147. Creating DOM Elements
+//==================================
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (movement, i) {
+    const transactionType = movement < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${transactionType}">${
+      i + 1
+    } ${transactionType}</div>
+        <div class="movements__value">$${Math.abs(movement)}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+    //console.log(html);
+  });
+};
+
+displayMovements(account1.movements);
+//console.log(containerMovements.innerHTML);
+
+//==================================
+// 148. Coding Challenge #1
+//==================================
+/*
+Coding Challenge #1
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners
+about their dog's age, and stored the data into an array (one array for each). For
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years
+old.
+
+
+4. Run the function for both test datasets
+Test data:
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+Hints: Use tools from all lectures in this section so far 😉
+GOOD LUCK 😀
+*/
+
+// Your tasks:
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  //1. Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+  // 2. Create an array with both Julia's (corrected) and Kate's data
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+
+  //3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+  allDogs.forEach(function (dog, i) {
+    const dogAge = dog >= 3 ? 'an adult' : 'still a puppy 🐶';
+    console.log(`Dog ${i + 1} is ${dogAge}, and is ${dog} years old`);
+  });
+};
+
+console.log(`*===== Test Data 1 =====*`);
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log(`*===== Test Data 2 =====*`);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
