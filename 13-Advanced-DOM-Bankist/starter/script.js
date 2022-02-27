5ecc40ccf561212b70d53d049c9e7905c22db821
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2022-02-27 15:45:04 -0500

    Complete  s186 -  select, create & delete (cont) - Complete section 186 - Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//================================================
+// 186. Selecting, Creating, and Deleting Elements
+//================================================
+//Selecting documents
+console.log(document.documentElement); //Shows entire html element
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections); //Will return a nodeList
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); //Will return an HTLMCollection or 'life-collection' (automaticall updays when DOM changes
+
+//Creating and inserting elements
+//.insertAdjacentHTML(position, HTMLString)
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'This was added via text content';
+message.innerHTML =
+  '<p>This was added via innerHTML</p><button class="btn btn--close-cookie">This button was also added with innerHTML</button>';
+
+header.prepend(message); //adds element as the first-child
+header.append(message); //adds element as the last-child
+//prepend and append not only inserts elements but it can also move them
+
+//If you want to insert multiple copies, you must first make a clone
+const messageClone = message.cloneNode(true);
+//header.prepend(messageClone);
+
+//before and affter adds the element as a sibling
+header.before(messageClone);
+//header.after(messageClone);
+
+//deleting elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    messageClone.remove();
+  });
